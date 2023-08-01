16be273c769f1887711a374e95e5d742025dd9e2
David Biersach <dbiersach@gmail.com>
2023-08-01 00:33:01 -0400

    Initial commit

--- Session 05 - Square Roots and Areas/instructor/newton_sqrt.py
+++ Session 05 - Square Roots and Areas/instructor/newton_sqrt.py
@@ -6,14 +6,12 @@ import numpy as np
 def sqrt(x):
     low, high = 0, x
     est = (low + high) / 2
-    n = 1
     while np.abs(est**2 - x) > 1e-10:
         if est**2 < x:
             low = est
         else:
             high = est
         est = (low + high) / 2
-        n = n + 1
     return est
 
 
