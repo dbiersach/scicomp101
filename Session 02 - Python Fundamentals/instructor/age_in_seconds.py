9d2351bcaa9ff7b153e347cfa0e442dd3750a1a5
David Biersach <dbiersach@gmail.com>
2023-10-17 01:55:45 -0400

    Update Task 12

--- Session 02 - Python Fundamentals/instructor/age_in_seconds.py
+++
@@ -1,7 +0,0 @@
-# age_in_seconds.py
-
-age_years = 56
-age_seconds = age_years * 365 * 24 * 60 * 60
-
-print(f"I am {age_years} years old", end=", ")
-print(f"which is {age_seconds:,} seconds.")
--- Session 05 - Square Roots and Areas/instructor/logarithm_series.py
+++
@@ -1,7 +0,0 @@
-# logarithm_series.py
-
-import numpy as np
-
-k = np.arange(1, 31)
-s = np.sum(1 / (2**k * k))
-print(np.exp(s))
