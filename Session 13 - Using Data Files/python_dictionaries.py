7774429de8d94f87694c2b0491c33e06fd165f1a
David Biersach <dbiersach@gmail.com>
2023-10-22 01:28:25 -0400

    Update Session 13

--- Session 13 - Using Data Files/python_dictionaries.py
+++ Session 13 - Using Data Files/python_dictionaries.py
@@ -2,7 +2,7 @@
 
 from pprint import pprint
 
-
+# Create a dictionary of key:value string pairs
 capitals = {
     "USA": "Washington D.C.",
     "Germany": "Berlin",
@@ -11,4 +11,20 @@ capitals = {
     "India": "New Delhi",
     "China": "Beijing",
 }
+pprint(capitals)
+print()
 
+# Two different ways to add a new key-value pair
+capitals.update({"United Kingdom": "London"})
+capitals["Spain"] = "Madrid"
+pprint(capitals)
+print()
+
+# Change an existing key's value
+capitals["USA"] = "New York"
+pprint(capitals)
+print()
+
+# Remove a key-value pair
+del capitals["Russia"]
+pprint(capitals)
