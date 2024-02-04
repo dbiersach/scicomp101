fec4a58048b4f2a3db90beef1fb512288a517484
Dave Biersach <dbiersach@gmail.com>
2024-02-03 20:55:24 -0500

    Switch to main()

--- Session 11 - 3D Graphics/plot3d_surface.py
+++ Session 11 - 3D Graphics/plot3d_surface.py
@@ -10,20 +10,26 @@ def f(x, y):
     return np.sin(np.sqrt(x**2 + y**2))
 
 
-x = np.linspace(-5, 5, 100)
-y = np.linspace(-5, 5, 100)
+def main():
+    x = np.linspace(-5, 5, 100)
+    y = np.linspace(-5, 5, 100)
 
-x, y = np.meshgrid(x, y)
-z = f(x, y)
+    x, y = np.meshgrid(x, y)
+    z = f(x, y)
 
-plt.figure("plot3d_surface.py", constrained_layout=True)
-ax = plt.axes(projection="3d")
-ax.figure.set_size_inches(10, 8)
+    plt.figure("plot3d_surface.py", constrained_layout=True)
+    plt.axes(projection="3d")
+    plt.gcf().set_size_inches(10, 8)
 
-surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
-plt.colorbar(surf, ax=ax, shrink=0.5, aspect=5)
+    surf = plt.gca().plot_surface(
+        x, y, z, cmap=cm.coolwarm, linewidth=0, antialiased=False
+    )
+    plt.colorbar(surf, ax=plt.gca(), shrink=0.5, aspect=5)
 
-ax.zaxis.set_major_locator(LinearLocator(10))
-ax.zaxis.set_major_formatter("{x:.02f}")
+    plt.gca().zaxis.set_major_locator(LinearLocator(10))
+    plt.gca().zaxis.set_major_formatter("{x:.02f}")
 
-plt.show()
+    plt.show()
+
+
+main()
