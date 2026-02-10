f998da6ca0086f0fbca2a924deb795e2f34318fd
Dave Biersach <dbiersach@gmail.com>
2026-02-10 16:00:28 -0500

    Force VSCode Extension installs

--- install_vscode_extensions.sh
+++ install_vscode_extensions.sh
@@ -1,19 +1,20 @@
-code --install-extension ms-vscode.cpptools
-code --install-extension ms-vscode.cpptools-extension-pack
-code --install-extension ms-vscode.powershell
-code --install-extension ms-python.python
-code --install-extension ms-python.vscode-pylance
-code --install-extension ms-toolsai.jupyter
-code --install-extension ms-toolsai.jupyter-renderers
-code --install-extension ms-python.isort
-code --install-extension streetsidesoftware.code-spell-checker
-code --install-extension ms-vscode.cmake-tools
-code --install-extension davidanson.vscode-markdownlint
-code --install-extension esbenp.prettier-vscode
-code --install-extension mechatroner.rainbow-csv
-code --install-extension emmanuelbeziat.vscode-great-icons
-code --install-extension james-yu.latex-workshop
-code --install-extension cschlosser.doxdocgen
-code --install-extension redhat.vscode-yaml
-code --install-extension charliermarsh.ruff
+code --install-extension ms-vscode.cpptools --force
+code --install-extension ms-vscode.cpptools-extension-pack --force
+code --install-extension ms-vscode.powershell --force
+code --install-extension ms-python.python --force
+code --install-extension ms-python.vscode-pylance --force
+code --install-extension ms-toolsai.jupyter --force
+code --install-extension ms-toolsai.jupyter-renderers --force
+code --install-extension ms-python.isort --force
+code --install-extension streetsidesoftware.code-spell-checker --force
+code --install-extension ms-vscode.cmake-tools --force
+code --install-extension davidanson.vscode-markdownlint --force
+code --install-extension esbenp.prettier-vscode --force
+code --install-extension mechatroner.rainbow-csv --force
+code --install-extension emmanuelbeziat.vscode-great-icons --force
+code --install-extension james-yu.latex-workshop --force
+code --install-extension cschlosser.doxdocgen --force
+code --install-extension redhat.vscode-yaml --force
+code --install-extension charliermarsh.ruff --force
+
 
