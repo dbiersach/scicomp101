93bff00a3b42400a28784a1f7e3f606f4a473fc9
Dave Biersach <dbiersach@gmail.com>
2026-02-25 08:51:22 -0500

    Update extensions and packages

--- install_vscode_extensions.sh
+++ install_vscode_extensions.sh
@@ -1,21 +1,27 @@
-#!/usr/bin/env bash
-code --install-extension ms-vscode.cpptools --force
-code --install-extension ms-vscode.cpptools-extension-pack --force
-code --install-extension ms-vscode.powershell --force
+code --install-extension charliermarsh.ruff --force
+code --install-extension cschlosser.doxdocgen --force
+code --install-extension davidanson.vscode-markdownlint --force
+code --install-extension emmanuelbeziat.vscode-great-icons --force
+code --install-extension esbenp.prettier-vscode --force
+code --install-extension github.copilot-chat
+code --install-extension james-yu.latex-workshop --force
+code --install-extension mechatroner.rainbow-csv --force
+code --install-extension ms-python.debugpy
+code --install-extension ms-python.isort --force
 code --install-extension ms-python.python --force
 code --install-extension ms-python.vscode-pylance --force
+code --install-extension ms-python.vscode-python-envs
 code --install-extension ms-toolsai.jupyter --force
+code --install-extension ms-toolsai.jupyter-keymap
 code --install-extension ms-toolsai.jupyter-renderers --force
-code --install-extension ms-python.isort --force
-code --install-extension streetsidesoftware.code-spell-checker --force
+code --install-extension ms-toolsai.vscode-jupyter-cell-tags
+code --install-extension ms-toolsai.vscode-jupyter-slideshow
 code --install-extension ms-vscode.cmake-tools --force
-code --install-extension davidanson.vscode-markdownlint --force
-code --install-extension esbenp.prettier-vscode --force
-code --install-extension mechatroner.rainbow-csv --force
-code --install-extension emmanuelbeziat.vscode-great-icons --force
-code --install-extension james-yu.latex-workshop --force
-code --install-extension cschlosser.doxdocgen --force
+code --install-extension ms-vscode.cpp-devtools
+code --install-extension ms-vscode.cpptools --force
+code --install-extension ms-vscode.cpptools-extension-pack --force
+code --install-extension ms-vscode.cpptools-themes
+code --install-extension ms-vscode.powershell --force
 code --install-extension redhat.vscode-yaml --force
-code --install-extension charliermarsh.ruff --force
-
-
+code --install-extension rust-lang.rust-analyzer
+code --install-extension streetsidesoftware.code-spell-checker --force
