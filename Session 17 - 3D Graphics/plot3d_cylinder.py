4d8a5290c57f6c2db468556e784f442ff8c9e441
Dave Biersach <dbiersach@gmail.com>
2026-04-15 22:18:20 -0400

    Add 3D Graphics

---
+++ Session 17 - 3D Graphics/plot3d_cylinder.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env -S uv run
+"""plot3d_cylinder_.py"""
+
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+radius, height = 10, 50
+
+u = np.linspace(0, height, 30)  # Vertical location
+v = np.linspace(0, 2 * np.pi, 30)  # Horizontal circular slice
+
+x = np.outer(u, v)
+y = np.outer(u, v)
+z = np.outer(u, v)
+
+plt.figure(Path(__file__).name)
+ax = plt.axes(projection="3d")
+ax.plot_surface(x, y, z, color="red")
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+ax.set_zlabel("z")
+ax.set_xlim(-height, height)
+ax.set_ylim(-height, height)
+ax.set_zlim(0, height)
+plt.show()
